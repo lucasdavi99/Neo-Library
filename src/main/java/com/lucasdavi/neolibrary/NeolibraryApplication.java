77802a136d6aafd48b6e0b3e8cd1c35e42bc4345
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2024-05-31 23:30:47 -0300

    first commit

---
+++ src/main/java/com/lucasdavi/neolibrary/NeolibraryApplication.java
@@ -0,0 +1,13 @@
+package com.lucasdavi.neolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeolibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeolibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasdavi/neolibrary/NeolibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdavi.neolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeolibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
