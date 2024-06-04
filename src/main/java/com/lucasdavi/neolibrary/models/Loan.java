9369f2a8277666be363ac436233684f2f0723e8c
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2024-06-03 21:17:49 -0300

    Criação das entidades Loan e User

---
+++ src/main/java/com/lucasdavi/neolibrary/models/Loan.java
@@ -0,0 +1,83 @@
+package com.lucasdavi.neolibrary.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Document(collection = "loans")
+public class Loan {
+
+    @Id
+    private String id;
+    private Book bookId;
+    private User userId;
+    private LocalDate loanDate;
+    private LocalDate returnDate;
+
+    public Loan() {
+    }
+
+    public Loan(String id, Book bookId, User userId, LocalDate loanDate, LocalDate returnDate) {
+        this.id = id;
+        this.bookId = bookId;
+        this.userId = userId;
+        this.loanDate = loanDate;
+        this.returnDate = returnDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Book getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Book bookId) {
+        this.bookId = bookId;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(LocalDate loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Loan loan = (Loan) o;
+        return Objects.equals(id, loan.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
