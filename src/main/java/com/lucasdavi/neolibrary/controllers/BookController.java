58992e44d822f68dc578a1854a3801ec4f4ac283
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2024-07-22 09:41:01 -0300

    Mod: Modificando estrutura geral do front na parte de livros

--- src/main/java/com/lucasdavi/neolibrary/controllers/BookController.java
+++ src/main/java/com/lucasdavi/neolibrary/controllers/BookController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -48,7 +45,7 @@ public class BookController {
     }
 
     @GetMapping("/{id}")
-    public String getBookById(Model model, Long id) {
+    public String getBookById(Model model, @PathVariable String id) {
         Book book = bookService.findById(String.valueOf(id));
         model.addAttribute("book", book);
         return "book";
